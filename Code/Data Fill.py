e34af31de8e9ff7000aba2f18557c5649d1effbd
ariramadh <ariramadhani24@gmail.com>
2022-09-30 18:50:59 +0700

    Fill na with Mean

--- Code/Data Fill.py
+++ Code/Data Fill.py
@@ -1,8 +1,11 @@
+from fileinput import filename
 from importlib.resources import path
 import pandas as pd
 import os
 
-csv_data_mean = pd.read_csv("Temperature_And_Precipitation_Cities_IN/Bangalore_1990_2022_BangaloreCity.csv")
+filename = "Rajasthan_1990_2022_Jodhpur"
+
+csv_data_mean = pd.read_csv("Temperature_And_Precipitation_Cities_IN/"+filename+".csv")
 csv_data_median = csv_data_mean
 
 print(csv_data_mean.head())
@@ -18,5 +21,5 @@ csv_data_median[['prcp']]=csv_data_median[['prcp']].fillna(0)
 print(csv_data_median.head())
 
 os.makedirs('Output', exist_ok=True)
-csv_data_mean.to_csv('Output/Bangalore_1990_2022_BangaloreCity_mean.csv',index=False)  
-csv_data_median.to_csv('Output/Bangalore_1990_2022_BangaloreCity_median.csv',index=False)
+csv_data_mean.to_csv('Output/'+filename+'_mean.csv',index=False)  
+csv_data_median.to_csv('Output/'+filename+'_median.csv',index=False)
